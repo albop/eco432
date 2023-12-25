51db698d2d2a06fe490b28efe7bfcc4afd4a78de
Pablo Winant <pablo.winant@gmail.com>
2023-12-25 13:46:58 +0100

    WIP

--- slides/session_1/ideas.md
+++
@@ -1,2 +0,0 @@
-for each group of economist:
-- add dates for important books
--- slides/session_3/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash 
-pandoc -t beamer -f markdown+implicit_figures index.md -o index.pdf --pdf-engine=xelatex --slide-level=3 --metadata=options
